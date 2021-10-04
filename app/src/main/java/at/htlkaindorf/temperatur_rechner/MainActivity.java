c2b54eb6c136bad8f90a61d86053313b104cbc36
Benjamin <kogbeb19@htl-kaindorf.at>
2021-10-04 11:35:27 +0200

    added onConvert

--- app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
@@ -3,13 +3,33 @@ package at.htlkaindorf.temperatur_rechner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText celsius;
+    private EditText kelvin;
+    private EditText fahrenheit;
+    private Button btconvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+
+
+    }
+
+    public void onConvert(){
+        if(celsius.getText().length() > 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() == 0){
+            float celsius = Float.parseFloat(celsius.getText().toString());
+            // + 273,15
+        } else if(celsius.getText().length() == 0 && kelvin.getText().length() > 0 && fahrenheit.getText().length() == 0){
+
+        } else if(celsius.getText().length() == 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() > 0){
+
+        }
     }
 }
