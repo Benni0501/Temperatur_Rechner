49ef42ae7731bd6ee08e5efa2f49441651918350
Benjamin <kogbeb19@htl-kaindorf.at>
2021-10-11 11:11:58 +0200

    edited OnConvert

--- app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void onConvert(){
         if(celsius.getText().length() > 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() == 0){
-            //float celsius = Float.parseFloat(celsius.getText().toString());
-            // + 273,15
+            float kelvin_help = Float.parseFloat(celsius.getText().toString()) + 273.15f;
+            float fahrenheit_help = Float.parseFloat(celsius.getText().toString()) + 32f;
+            kelvin.setText(kelvin_help);
+            fahrenheit.setText(fahrenheit_help);
         } else if(celsius.getText().length() == 0 && kelvin.getText().length() > 0 && fahrenheit.getText().length() == 0){
-
+            float celsius_help = Float.parseFloat(kelvin.getText().toString()) - 237.15f;
+            float fahrenheit_help = Float.parseFloat(kelvin.getText().toString()) - 459.67f;
+            celsius.setText(celsius_help);
+            fahrenheit.setText(fahrenheit_help);
         } else if(celsius.getText().length() == 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() > 0){
-
+            float celsius_help = Float.parseFloat(fahrenheit.getText().toString()) - 17.7778f;
+            float kelvin_help = Float.parseFloat(fahrenheit.getText().toString()) + 255.372f;
+            celsius.setText(celsius_help);
+            kelvin.setText(kelvin_help);
+        } else {
+            Toast.makeText(getApplicationContext(), "Bitte nur ein Zahl eingeben", Toast.LENGTH_LONG).show();
         }
     }
 }
