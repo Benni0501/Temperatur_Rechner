a784d773b2d62afd951c4f6a32adbc7a05586d3f
Benjamin <kogbeb19@htl-kaindorf.at>
2021-10-11 11:32:39 +0200

    Den Job von Raffael gemacht

--- app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperatur_rechner/MainActivity.java
@@ -3,6 +3,7 @@ package at.htlkaindorf.temperatur_rechner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,24 +26,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onConvert(){
+    public void onConvert(View view){
         if(celsius.getText().length() > 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() == 0){
             float kelvin_help = Float.parseFloat(celsius.getText().toString()) + 273.15f;
             float fahrenheit_help = Float.parseFloat(celsius.getText().toString()) + 32f;
-            kelvin.setText(kelvin_help);
-            fahrenheit.setText(fahrenheit_help);
+            kelvin.setText(getString(R.string.input, kelvin_help));
+            fahrenheit.setText(getString(R.string.input, fahrenheit_help));
+
         } else if(celsius.getText().length() == 0 && kelvin.getText().length() > 0 && fahrenheit.getText().length() == 0){
-            float celsius_help = Float.parseFloat(kelvin.getText().toString()) - 237.15f;
+            float celsius_help = Float.parseFloat(kelvin.getText().toString()) - 273.15f;
             float fahrenheit_help = Float.parseFloat(kelvin.getText().toString()) - 459.67f;
-            celsius.setText(celsius_help);
-            fahrenheit.setText(fahrenheit_help);
+            celsius.setText(getString(R.string.input, celsius_help));
+            fahrenheit.setText(getString(R.string.input, fahrenheit_help));
+
         } else if(celsius.getText().length() == 0 && kelvin.getText().length() == 0 && fahrenheit.getText().length() > 0){
             float celsius_help = Float.parseFloat(fahrenheit.getText().toString()) - 17.7778f;
             float kelvin_help = Float.parseFloat(fahrenheit.getText().toString()) + 255.372f;
-            celsius.setText(celsius_help);
-            kelvin.setText(kelvin_help);
+            celsius.setText(getString(R.string.input, celsius_help));
+            kelvin.setText(getString(R.string.input, kelvin_help));
         } else {
             Toast.makeText(getApplicationContext(), "Mach das bitte net", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void onClear(View view){
+        kelvin.setText("");
+        fahrenheit.setText("");
+        celsius.setText("");
     }
 }
